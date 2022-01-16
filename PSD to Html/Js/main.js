61ed992188a8e9ef6c21593c393036eff15a8084
skaman290 <96546531+skaman290@users.noreply.github.com>
2022-01-16 23:50:00 +0600

    Add files via upload

---
+++ PSD to Html/Js/main.js
@@ -0,0 +1,9 @@
+// Bx slider
+$(document).ready(function () {
+    $('.slider').bxSlider();
+    $('#main_menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+
+});
